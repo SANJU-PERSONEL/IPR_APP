d1c19b8fa3713366415f65f8b0b2a3abff532497
Sanju Toor <Sanju.Toor@symphonyteleca.com>
2017-08-09 15:27:06 +0530

    Initial Commit
     Author:    Sanju Toor <sanjutoor@gmail.com>

---
+++ IPR_SimpleApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPR_SimpleApplication
+//
+//  Created by Toor, Sanju on 09/08/17.
+//  Copyright © 2017 SANJU-PERSONEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IPR_SimpleApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IPR_SimpleApplication
+//
+//  Created by Toor, Sanju on 09/08/17.
+//  Copyright © 2017 SANJU-PERSONEL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
